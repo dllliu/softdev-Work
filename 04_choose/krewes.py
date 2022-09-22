904cd9fb19396022ae87e64c3b6ae43bf7ba4123
dliu <dliu30@stuy.edu>
2022-09-22 09:24:18 -0400

    add functions and summary

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,28 +1,32 @@
 # Ryan Lau WeiChen Liu Daniel Liu
 #SoftDev
-
+#K04-DictWord
 #2022-09-22
-#Time Spent:
+#Time Spent: 0.4 Hour
 
 
 """
 DISCO:
 -random.randint() accepts two arguements, inclusive of both
+-You Can Access The Value Pair By Using The Key as the "Index"
 
 QCC:
-
+-How Can We Manually Generate Random-Ness Without Importing Random?
 
 OPS SUMMARY:
-
+-First we wrote the heading. Then we initialized the dictionary of
+period:students as the key:value pairs. Then we imported random and
+then made functions to first choose the random period, then another
+function to return a random student or ducky based on the output of the random period function.
 """
 
 import random
 
-krewes = {2:["WeiChen"],7:["Ryan"],8:["Daniel"]}
+krewes = {2:["WeiChen","David"],7:["Ryan","<T>"],8:["Daniel","Porky"]}
 
-def select_random():
+def select_random_period():
     rand = random.randint(0,2)
-    print(rand)
+    #print(rand)
     temp = 0
     if rand == 0:
         temp = 2
@@ -32,5 +36,12 @@ def select_random():
         temp = 8
     return krewes[temp]
 
-print(select_random())
+
+def select_random_student(students):
+    rand = random.randint(0,len(students)-1)
+    return students[rand]
+
+    
+#print(select_random_period())
+print(select_random_student(select_random_period()))
     
