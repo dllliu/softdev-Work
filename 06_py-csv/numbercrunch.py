632e57488066ac2ad7087b95245cde198d9c4d96
dliu <dliu30@stuy.edu>
2022-10-03 21:53:02 -0400

    add support for new test case file for 06 and added more test cases

--- 06_py-csv/numbercrunch.py
+++ 06_py-csv/numbercrunch.py
@@ -10,18 +10,26 @@ DISCO:
 -csv.reader() function takes in params like delimiter and quotechar
 -delimiter specifies the character used to separate each field. The default is the comma (',')
 -quotechar specifies the character used to surround fields that contain the delimiter character. The default is a double quote (' " ').
-
+on creating own random:
+- 2 methods one creating a cumulative sum and dividing parts of it another creates multiple entries of a job in a list depending on percentage
+- random.uniform()
 QCC:
 What type/object does csv.reader() return?
-
 OPS SUMMARY:
 -Uses csv.reader() to parse csv, seperates each field by "," or '"' for fields that contain a comma
--loop through each line and construct dict of occupation:percent 
+-Csv.reader() will only read the second row to the second to last row
+-loop through each line and construct dict of occupation:percent
+V1:
 -our random weight function works by checking if associated percent is 100. If not, find associated percent and multiply by 10. Append the percent 
 to the array that number of times. Repeat for every key (occupation)
 -Use randm.choice to get a "weighted" percent
+V2:
+- adds jobs to a new dictionary that uses the cumulative sum (previous sum + percentage) as the key
+- random.uniform() generates a random number from 0 to final sum
+- checks through dictionary keys for whether in range and returns the associate value of the key that the randNum is less than
 """
 
+#imports
 import csv
 import random 
 
@@ -29,20 +37,26 @@ dict = {}
 
 with open("occupations.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",",quotechar='"')
-    for lines in csvreader:
+    rows_arr = list(csvreader)
+    rows_arr = rows_arr[1:-1]
+    for lines in rows_arr:
         occupation = lines[0]
         percent = float(lines[1])
         dict[occupation] = percent
     all_jobs = list(dict.keys())
     all_percents = list(dict.values())
-
+    print(dict)
 
 def doRandomWeights():
+    #creates list and total slices
     arr = []
     total = 100
+    # adds the job to the list corresponding number of times as percentage
+    #(legal, legal, legal, education training and library, education training and library)
     for job in all_jobs:
         if dict[job] != total:
             percent = dict[job]
+            #takes percent and multiplies by 10 to get rid of percent
             weighted = int(percent * 10)
         else:
             arr.append(job)
@@ -50,7 +64,30 @@ def doRandomWeights():
         for i in range(weighted):
             arr.append(job)
     return random.choice(arr)
+
+
+def doRandomWeights2():
+    #create sum var and new dict
+    sums = 0
+    weights = {}
+    #goes through jobs and makes cumulative sum to add to weights
+    for job in all_jobs:
+        percent = dict[job]
+        sums += percent
+        weights[sums] = job
+    choice = random.uniform(0, sums)
+    #goes through weights and if randNum is in range returns that entry
+    for key in weights:
+        if(choice < key):
+            return weights[key]
+    return 0
+        
+    
         
 #randomWeightedJob = random.choices(all_jobs,weights=all_percents)
 #print(randomWeightedJob)
-print(doRandomWeights())
+for i in range(50):
+    print("Output From The First Algorithm for Random Weights Is " + doRandomWeights())
+
+for i in range(50):
+    print("Output From The Second Algorithm for Random Weights Is " + doRandomWeights2())
--- 06_py-csv/withoutCSV.py
+++ 06_py-csv/withoutCSV.py
@@ -1,4 +1,4 @@
-#Talia Daniel Dynamic Turtles
+#Talia Daniel
 #SoftDev
 #06
 #2022-09-28
@@ -10,9 +10,11 @@ atom adds a new line upon save (extra time to debug because i edited csv)
 intro usage of .replace() and .strip()
 how to include weights in random selection
 typecasting converts the entire list value not just contents meaning the list syntax remains
+
 QCC:
+
 OPS SUMMARY:
-- opens file and reads content into a string variable
+- opens file from second line to second to last line and reads content into a string variable
 - splits the string by \n using .split()
 - splits each entry differently based on whether it has a quotation mark or a comma
 - created dictionary using percentages as keys and names of jobs as values
@@ -21,14 +23,20 @@ OPS SUMMARY:
 - chose a job using random.choices() with the weights attribute
 - turned selection into a string removing list syntax and printed in an fstring
 """
-
 import random
 
 dict = {}
 
+def remove_lines(str):
+    idx_first = str.find("\n")
+    idx_last = str.rfind("\n")
+    return str[idx_first+1:idx_last]
+    
+    
 file1 = open("occupations.csv")
 string = file1.read()
-each_entry = string.split("\n")
+cutString = remove_lines(string)
+each_entry = cutString.split("\n")
 for i in each_entry:
     if '"' in i:
         parsed_arr = i.strip('"').split('"')
@@ -36,12 +44,15 @@ for i in each_entry:
         parsed_arr = i.split(",")
     occupation = parsed_arr[0]
     percentage = float(parsed_arr[1].replace(",",""))
-    dict[percentage] = occupation
+    dict[percentage] = []
+    dict[percentage].append(occupation)
 
-    
-print(dict)
 
-all_percents = list(dict.keys())
-all_occupations_arr = list(dict.values())
-randomWeightedJob = random.choices(all_occupations_arr,weights=all_percents)
-print(randomWeightedJob)
+for i in range(100):
+    all_percents = tuple(dict.keys())
+    all_occupations_arr = list(dict.values())
+    randomWeightedPercent = random.choices(all_occupations_arr,weights=(all_percents))
+    stringV = str(randomWeightedPercent).strip("[").strip("]").strip("'")
+    print(f"{stringV}")
+
+#print(dict[all_percents[random.randrange(len(all_percents))]])
