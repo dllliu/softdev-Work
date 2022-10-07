feb10897c65838788758b929ea0a42356dbd8e2b
dliu <dliu30@stuy.edu>
2022-10-07 09:01:05 -0400

    finalize

--- 08_serve/app.py
+++ 08_serve/app.py
@@ -1,14 +1,21 @@
 # 10 items or fewer express checkout lane: Daniel Liu, Anthony Sun, Jasmine Yuen
 # SoftDev
 # 2022-10-06
-#Time Spent: 0.4 Hour
+#Time Spent: 1 Hour
 
 """
 DISCO:
+* how to use flask
+* we can render html thru the return
+* csv library is very useful to parse csv files
 
 QCC:
+* Why does app.debug = True give us an error? (/usr/bin/python3: No module named thonny.plugins.cpython.app)
 
 OPS SUMMARY:
+* We refactored our numbercruncher.py file to return a dictionary that contains jobs and their weights / generate a weighted random job
+* We then followed the flask templates from 00_flask
+* We made the information more presentable by wrapping it with HTML
 
 """
 
@@ -21,8 +28,8 @@ def disp_random_occ():
     jobDict = numbercruncher.generate_dict("occupations.csv") 
     s = ""
     s += "<h1 >10 items or fewer express checkout lane </h1> <h2> Daniel Liu, Anthony Sun, Jasmine Yuen </h2>"
-    s += "<p>Your Best Bet Is the " + "<i>" + str(numbercruncher.give_weighted_job(jobDict)) + "</i>" + " Job" + "</p>"
-    s += "All The Occupations Are: " + numbercruncher.get_all_occupations(jobDict)
+    s += "<p>Your best bet is the " + "<i style = \"color: #006AFF\">" + str(numbercruncher.give_weighted_job(jobDict)) + "</i>" + " job" + "</p>"
+    s += "All the occupations are: " + numbercruncher.get_all_occupations(jobDict)
     return s
 
 app.debug = True
