eea06cff57ef62c8f6522c74b4d960cbb8e256f1
dliu <dliu30@stuy.edu>
2022-10-18 16:01:17 -0400

    add blurb

--- 12_flask-forms/app.py
+++ 12_flask-forms/app.py
@@ -26,26 +26,11 @@ def disp_loginpage():
     #print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
-    return render_template( 'login.html' )
-
-
-@app.route("/auth", methods=['GET', 'POST'])
-def authenticate():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    print("----------------")
-    print(request.form)
-    return render_template('response.html')
-
+    
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        return render_template('response.html', method = request.method, username = request.form['username'])
 
     
 if __name__ == "__main__": #false if this file imported as module
