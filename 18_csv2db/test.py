7860ad7cfa033dd6a980b59c1af37876a68fb528
dliu <dliu30@stuy.edu>
2022-10-26 09:43:05 -0400

    add

--- 18_csv2db/test.py
+++ 18_csv2db/test.py
@@ -1,4 +1,5 @@
 import csv
+import random 
 
 with open("students.csv","r") as filename:
     dictreader = csv.reader(filename)
@@ -6,8 +7,30 @@ with open("students.csv","r") as filename:
         for i in range(len(row)):
             if row[i].isnumeric():
                 row[i]= int(row[i])
-        print(tuple(row))
+        #print(tuple(row))
 
             
+krewes = [
+           ["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY"], 
+           ["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+           ["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "WANYING"]]
 
+def read_krewes():
+    str = ""
+    for arr in krewes:
+        for name in arr:
+            str += name + ", "
+    return str[:len(str)-2]
+
+ages = []
+for i in range(len(read_krewes())):
+    ages.append(17)
+
+ans =[]
+for item in krewes[0]:
+    #print(item)
+    for item_1 in ages:
+        ans.append(item)
+        ans.append(item_1)
+print(ans)
 
