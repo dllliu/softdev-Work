081196cd1927835bce93888cbddce34247efecc3
dliu <dliu30@stuy.edu>
2022-11-03 14:33:19 -0400

    works

--- 19_session/app.py
+++ 19_session/app.py
@@ -1,12 +1,12 @@
 # ADS: Ayman Habib, Sam Lubelsky, Daniel Liu
 # Softdev pd02
-# k12
-# 2022-10-17
-# time spent: 0.3 hr
+# k19
+# 2022-11-03
+# time spent: 1.0 hr
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template, make_response   #facilitate jinja templating
-from flask import request           #facilitate form submission
+from flask import request, session, redirect, url_for          #facilitate form submission
 import os 
 
 #the conventional way:
@@ -16,21 +16,22 @@ app = Flask(__name__)    #create Flask object
 app.secret_key = os.urandom(32)
 
 
-#session[key] = value
-#session.pop(key)
 username = "ads"
 password = "admin"
 
 @app.route('/', methods=['GET','POST'])
-def disp_loginpage():
+def login():
+    if username in session:
+        return f'Logged in as {session["username"]}'
     return render_template('login.html')
+    
+'''
+    session['username'] = request.form.get('username')
+    return render_template('login.html')
+'''
 
-
-@app.route("/response", methods=['GET', 'POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def authenticate():
-    print("COOKIE INFO")
-    print(request.cookies.get("username"))
-    print(request.form.get("password"))
     userIn = request.form.get('username')
     passIn = request.form.get('password')
     if(userIn != username):
@@ -40,14 +41,16 @@ def authenticate():
         resp = make_response(render_template("error.html", msg = "wrong password"))
         return resp 
     else:
-        resp = make_response(render_template('response.html',username=userIn))
-        resp.set_cookie('username', userIn)
-        return resp
+        session['username'] = request.form['username']
+        #resp = make_response(render_template('response.html',username=userIn))
+        #return resp
+        return render_template('response.html',username = userIn)
 
-@app.route("/logout", methods=['GET', 'POST'])
+@app.route('/logout')
 def logout():
-    session[key] = value
-    session.pop(key)
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('login'))
     
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
