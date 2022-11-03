16910b996f96a3030321a2fbc8ed066076f9346a
dliu <dliu30@stuy.edu>
2022-11-03 19:29:34 -0400

    fix session

--- 19_session/app.py
+++ 19_session/app.py
@@ -1,12 +1,12 @@
 # ADS: Ayman Habib, Sam Lubelsky, Daniel Liu
 # Softdev pd02
-# k19
+# k1
 # 2022-11-03
 # time spent: 1.0 hr
 
 from flask import Flask             #facilitate flask webserving
-from flask import render_template, make_response   #facilitate jinja templating
-from flask import request, session, redirect, url_for          #facilitate form submission
+from flask import render_template, request   #facilitate jinja templating
+from flask import session, redirect, url_for, make_response        #facilitate form submission
 import os 
 
 #the conventional way:
@@ -15,44 +15,46 @@ import os
 app = Flask(__name__)    #create Flask object
 app.secret_key = os.urandom(32)
 
-
 username = "ads"
 password = "admin"
 
-@app.route('/', methods=['GET','POST'])
-def login():
-    if username in session:
-        return f'Logged in as {session["username"]}'
+@app.route('/')
+def index():
+    if 'username' in session:
+        return render_template('response.html',username = session['username'])
     return render_template('login.html')
-    
-'''
-    session['username'] = request.form.get('username')
-    return render_template('login.html')
-'''
 
-@app.route("/login", methods=['GET', 'POST'])
-def authenticate():
-    userIn = request.form.get('username')
-    passIn = request.form.get('password')
-    if(userIn != username):
-        resp = make_response(render_template("error.html", msg = "wrong username"))
-        return resp
-    elif passIn != password:
-        resp = make_response(render_template("error.html", msg = "wrong password"))
-        return resp 
-    else:
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    '''
+    if request.method == 'POST':
         session['username'] = request.form['username']
-        #resp = make_response(render_template('response.html',username=userIn))
-        #return resp
-        return render_template('response.html',username = userIn)
+        return redirect(url_for('index'))
+    '''
+    if request.method == 'POST':
+        userIn = request.cookies.get('username')
+        passIn = request.form.get('password') 
+        if(userIn != username):
+            resp = make_response(render_template("error.html", msg = "wrong username"),404)
+            return resp
+        elif passIn != password:
+            resp = make_response(render_template("error.html", msg = "wrong password"),404)
+            return resp
+        else:
+            session['username'] = request.form['username']
+            resp = render_template('response.html',username = session['username'])
+            return resp
+    return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
+
     
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
     app.run()
+    
