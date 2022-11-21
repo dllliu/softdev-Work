51202395562b3229d3ad651b46d1157fbe3cec68
dliu <dliu30@stuy.edu>
2022-11-21 17:03:26 -0500

    finish 20

---
+++ 20_restapi/app.py
@@ -0,0 +1,28 @@
+# TEAM NAME??? Daniel, Nicholas
+# Softdev pd02
+# k20
+# 2022-11-21
+# time spent: 0.6 hr
+
+from flask import *
+import requests
+
+nasa_url = "https://api.nasa.gov/planetary/apod?api_key="
+
+f = open("key_nasa.txt","r")
+key = f.read()
+complete_url = nasa_url + key
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/")
+def index():
+    r = requests.get(complete_url).json()
+    img_url = r["url"]
+    img_exp = r["explanation"]
+    return render_template("main.html",img_url = img_url, img_exp = img_exp)
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
---
+++ 20_restapi/readme.md
@@ -0,0 +1,8 @@
+### DISCO
+
+* use requests.get({{some_url}}).json() to get the json object from a specified web url
+* access specifics within the json object almost like a key-value pair
+
+### QCC
+
+* Is using the requests library the best way to accomplish this task?
