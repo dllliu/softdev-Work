51fc86dc82a38b6e4d31926e4489dc91ecb3cd60
dliu <dliu30@stuy.edu>
2023-04-25 17:27:02 -0400

    add hw

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -1,38 +1,59 @@
-var c = document.getElementById("playground");
-var dotButton = document.getElementById("buttonCircle");
-var stopButton = document.getElementById("buttonStop");
+// Team Aggregate Dinosaurs :: Anjini Katari, Daniel Liu
+// SoftDev pd7
+// K31 JS Paint, Paint, Paint...
+// 2023-04-25
+// --------------------------------------------------
+
+var c = document.getElementById("playground"); //GET CANVAS
+var dotButton = document.getElementById("buttonCircle"); //GET DOT BUTTON
+var stopButton = document.getElementById("buttonStop"); //GET STOP BUTTON
 
 var ctx = c.getContext("2d");
 
 ctx.fillStyle = "green";
-var requestID; 
+var requestID; //init global var for use with animation frames
 
 var clear = (e) => {
     ctx.clearRect(0,0,c.width,c.height);
 };
 
-var radius = 0;
+var radius = 100;
+var radius_inc = 1;
 var growing = true;
 
-var drawCircle = function(e) {
-    console.log("drawing circle...")
+var drawCircle = () => {
     ctx.beginPath();
     ctx.fillStyle = "red";
-    ctx.arc(0,0,50,50,0,2 * Math.PI);
+    ctx.arc(c.width/2,c.height/2,radius,0,2 * Math.PI);
     ctx.stroke()
     ctx.fill()
  }
 
 var drawDot = (e) => {
-   //console.log(requestID);
-   clear;
-   drawCircle;
-   window.requestAnimationFrame(e);
+    requestID = window.requestAnimationFrame(drawDot);
+    clear();
+    drawCircle();
+    if (growing) {
+        if (radius > c.width / 2) { //if too big
+            growing = false;
+        }
+        radius += radius_inc;
+    } 
+    else {
+        radius -= radius_inc;
+        if (radius == 0) {  //other side when growing is set to false
+            growing = true
+        }
+    }
 };
 
+var once = true;
 var stopIt = () => {
-
+    console.log("stopIt invoked...");
+    console.log(requestID);
+    window.cancelAnimationFrame(requestID);
 };
 
-dotButton.addEventListener("click",drawDot);
-stopButton.addEventListener("click",stopIt);
+dotButton.addEventListener("click",drawDot,once);
+
+stopButton.addEventListener("click",stopIt,once);
